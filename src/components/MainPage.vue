5bee92cc4aafbd55f6e92a66e931e3400f7b1710
hopcv <hopcv58@gmail.com>
2022-11-10 21:28:54 +0700

    building

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -12,12 +12,15 @@
       <input type="number" id="weight" v-model="weight"/>
     </div>
     <div class="input">
-      <label for="age">Age:</label>
-      <input type="number" id="age" v-model="age"/>
-    </div>
-    <div class="input">
       <label for="height">Height (cm):</label>
       <input type="number" id="height" v-model="height"/>
+    </div>
+    <div class="input">
+      <label for="gender">Gender:</label>
+      <select id="gender" v-model="gender">
+        <option value="Male">Male</option>
+        <option value="Female">Female</option>
+      </select>
     </div>
     <button @click="predict" class="button">Predict</button>
   </div>
@@ -34,45 +37,171 @@ export default {
     return {
       height: 0,
       weight: 0,
-      age: 0,
+      gender: 'Male',
       size: 0,
     }
   },
   created() {
     window.trained = false;
+
+    window.minMaleWeight = 112.903;
+    window.minMaleWeightKg = 45;
+    window.maxMaleWeightDiff = 269.99 - 112.903;
+    window.maxMaleWeightKgDiff = 90-55;
+
+    window.minMaleHeight = 58.4069
+    window.minMaleHeightCm = 147;
+    window.maxMaleHeightDiff = 78.9987 - 58.4069;
+    window.maxMaleHeightCmDiff = 190-147;
+
+    window.minFemaleWeight = 64.7001;
+    window.minFemaleWeightKg = 30;
+    window.maxFemaleWeightDiff = 202.237 - 64.7001;
+    window.maxFemaleWeightKgDiff = 80-30;
+
+    window.minFemaleHeight = 54.2631
+    window.minFemaleHeightCm = 130;
+    window.maxFemaleHeightDiff = 73.3896 - 54.2631;
+    window.maxFemaleHeightCmDiff = 180-130;
   },
   methods: {
     train: async () => {
+      if (window.trained) {
+        alert('Classifier already trained!');
+        return;
+      }
       let bayes = require('node-bayes');
-      const response = await fetch('/train.csv');
+      const response = await fetch('/weight-height.csv');
       const data = await response.blob();
       const file = new File([data], name, {
         type: data.type || "text/plain",
       });
       let columnArr = [];
-      let dataArr = [];
+      let dataArrForMale = [];
+      let dataArrForFemale = [];
       const reader = new FileReader();
       reader.readAsText(file);
       reader.onload = () => {
         const lines = reader.result.split('\r\n');
         lines.forEach(line => {
-          const [weight, age, height, size] = line.split(',');
-          if (weight === 'weight') {
-            columnArr = [weight, age, height, 'bmi', 'massSquared', size];
+          const [gender, height, weight] = line.split(',');
+          if (gender === '"Gender"') {
+            columnArr = ['Weight', 'Height', 'Size'];
             console.log(columnArr);
-          } else if (weight && age && height && size) {
-            let bmi = weight / ((height / 100) ** 2);
-            let massSquared = weight ** 2;
-            dataArr.push([parseFloat(weight), parseFloat(age), Math.round(parseFloat(height)), bmi, massSquared, size]);
-            console.log([parseFloat(weight), parseFloat(age), Math.round(parseFloat(height)), bmi, massSquared, size])
+          } else if (weight && gender && height) {
+            if (gender === '"Male"') {
+              let weightDiffWithMin = weight - window.minMaleWeight;
+              let weightKg = weightDiffWithMin * window.maxMaleWeightKgDiff / window.maxMaleWeightDiff + window.minMaleWeightKg;
+              let heightDiffWithMin = height - window.minMaleHeight;
+              let heightCm = heightDiffWithMin * window.maxMaleHeightCmDiff / window.maxMaleHeightDiff + window.minMaleHeightCm;
+              let size = '';
+
+              weightKg = Math.round(weightKg);
+              heightCm = Math.round(heightCm);
+
+              if (weightKg < 55) {
+                if (heightCm < 160) {
+                  size = 'XS';
+                } else {
+                  size = 'S';
+                }
+              } else if (weightKg < 60) {
+                if (heightCm < 165) {
+                  size = 'S';
+                } else {
+                  size = 'M';
+                }
+              } else if (weightKg < 65) {
+                if (heightCm < 170) {
+                  size = 'M';
+                } else {
+                  size = 'L';
+                }
+              } else if (weightKg < 70) {
+                if (heightCm <= 174) {
+                  size = 'L';
+                } else {
+                  size = 'XL';
+                }
+              } else if (weightKg <= 76) {
+                if (heightCm <= 176) {
+                  size = 'XL';
+                } else {
+                  size = 'XXL';
+                }
+              } else if (weightKg < 80) {
+                if (heightCm <= 177) {
+                  size = 'XXL';
+                } else {
+                  size = 'XXXL';
+                }
+              } else {
+                size = 'XXXL'
+              }
+              dataArrForMale.push([weightKg, heightCm, size]);
+            } else {
+              let weightDiffWithMin = weight - window.minFemaleWeight;
+              let weightKg = weightDiffWithMin * window.maxFemaleWeightKgDiff / window.maxFemaleWeightDiff + window.minFemaleWeightKg;
+              let heightDiffWithMin = height - window.minFemaleHeight;
+              let heightCm = heightDiffWithMin * window.maxFemaleHeightCmDiff / window.maxFemaleHeightDiff + window.minFemaleHeightCm;
+              let size = '';
+
+              weightKg = Math.round(weightKg);
+              heightCm = Math.round(heightCm);
+
+              if (weightKg < 38) {
+                if (heightCm < 145) {
+                  size = 'XS';
+                } else {
+                  size = 'S';
+                }
+              } else if (weightKg < 43) {
+                if (heightCm < 153) {
+                  size = 'S';
+                } else {
+                  size = 'M';
+                }
+              } else if (weightKg < 46) {
+                size = 'M';
+              } else if (weightKg < 53) {
+                size = 'L';
+              } else if (weightKg <= 57) {
+                size = 'XL';
+              } else if (weightKg < 66) {
+                size = 'XXL';
+              } else {
+                size = 'XXXL'
+              }
+              dataArrForFemale.push([weightKg, heightCm, size]);
+            }
           }
         });
-        window.cls = new bayes.NaiveBayes({
+
+        let string = ''
+        for (let i = 0; i <= dataArrForMale.length; i++) {
+          if (dataArrForMale[i])
+          string = string + 'Male,' + dataArrForMale[i].join(',') + '\n';
+        }
+        for (let i = 0; i <= dataArrForFemale.length; i++) {
+          if (dataArrForFemale[i])
+          string = string + 'Female,' + dataArrForFemale[i].join(',') + '\n';
+        }
+        console.log(string);
+
+        window.maleClassifier = new bayes.NaiveBayes({
           columns: columnArr,
-          data: dataArr,
+          data: dataArrForMale,
           verbose: true
         });
-        window.cls.train();
+        window.maleClassifier.train();
+
+        window.femaleClassifier = new bayes.NaiveBayes({
+          columns: columnArr,
+          data: dataArrForFemale,
+          verbose: true
+        });
+        window.maleClassifier.train();
+        window.femaleClassifier.train();
         alert('Training complete');
         window.trained = true;
       }
@@ -84,12 +213,15 @@ export default {
       }
       window.weight = this.weight;
       window.height = this.height;
-      window.age = this.age;
-      let bmi = window.weight / ((window.height / 100) ** 2);
-      let massSquared = window.weight ** 2;
-      let answer = window.cls.predict([window.weight, window.age, window.height, bmi, massSquared]);
-      alert('The prediction for weight ' + window.weight + ', age ' + window.age + ', height ' + window.height + ' is ' + answer.answer);
-      console.log([window.weight, window.age, window.height], answer);
+      window.gender = this.gender;
+      let answer
+      if (this.gender === 'Male') {
+        answer = window.maleClassifier.predict([window.weight, window.height]);
+      } else {
+        answer = window.femaleClassifier.predict([window.weight, window.height]);
+      }
+      alert('The prediction for weight ' + window.weight + ', height ' + window.height + ', gender ' + window.gender + ' is ' + answer.answer);
+      console.log([window.weight, window.height], answer);
     }
   }
 }
